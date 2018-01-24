a8a2f87b603b24740f5adcd1d2d566ee8586a78d
Krystian Kulawiak <krystian.k907@gmail.com>
2018-01-24 18:13:34 +0100

    zmiana predkosci rakiety

--- gravityRunner/classes/Scene Classes/GameplayScene.swift
+++ gravityRunner/classes/Scene Classes/GameplayScene.swift
@@ -154,7 +154,7 @@ override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     private func moveRocket() {
         enumerateChildNodes(withName: "Rocket", using: ({
             (node, error) in
-            node.position.x -= 5;
+            node.position.x -= 4;
         }))
     }
     
