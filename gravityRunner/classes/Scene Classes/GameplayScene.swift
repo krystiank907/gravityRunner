c4adc1e23b88df24afbae540b834c1c25ed760f2
Krystian Kulawiak <krystian.k907@gmail.com>
2018-01-24 18:15:03 +0100

    zmiana predkosci playera i kamery

--- gravityRunner/classes/Scene Classes/GameplayScene.swift
+++ gravityRunner/classes/Scene Classes/GameplayScene.swift
@@ -114,7 +114,7 @@ override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     }
     
     private func manageCamera() {
-        self.mainCamera?.position.x += 15;
+        self.mainCamera?.position.x += 10;
     }
     
     private func manageBGsAndGrounds() {
